b70ac26cae922c2f6b9b37868df410541d13f7d3
Mihanchos <shofer2007@gmail.com>
2024-10-09 14:46:13 +0300

    new tasks commiting

---
+++ Informatics/collections.py
@@ -0,0 +1,45 @@
+# def input_matrix():
+#     size = input()
+#     rows, cols = map(int, size.split('x'))
+#     matrix = [list(map(int, input().split())) for _ in range(rows)]
+#     return matrix
+#
+# matrix1 = input_matrix()
+# matrix2 = input_matrix()
+#
+# rows1, cols1 = len(matrix1), len(matrix1[0]) if matrix1 else 0
+# rows2, cols2 = len(matrix2), len(matrix2[0]) if matrix2 else 0
+#
+# result_rows = max(rows1, rows2)
+# result_cols = max(cols1, cols2)
+#
+# result_matrix = [[0] * result_cols for _ in range(result_rows)]
+#
+# for i in range(result_rows):
+#     for j in range(result_cols):
+#         val1 = matrix1[i][j] if i < rows1 and j < cols1 else 0
+#         val2 = matrix2[i][j] if i < rows2 and j < cols2 else 0
+#         result_matrix[i][j] = val1 + val2
+#
+# print(result_matrix)
+
+# n, m = map(int, input().split())
+# matrix = [[0] * m for _ in range(n)]
+# num = 1
+#
+# for diag in range(n + m - 1):
+#     row, col = (0, diag) if diag < m else (diag - m + 1, m - 1)
+#     while row < n and col >= 0:
+#         matrix[row][col] = num
+#         num += 1
+#         row += 1
+#         col -= 1
+#
+# max_len = len(str(num - 1))
+# for row in matrix:
+#     print(*[f"{x:<{max_len}}" for x in row])
+
+# row, n = [1], int(input())
+# for k in range(1, n + 1):
+#     row.append(row[-1] * (n - k + 1) // k)
+# print(row)
--- NTO2024/NTO.py
+++
@@ -1,9 +0,0 @@
-gribs_live = [4]
-for hours in range(1, 25):
-    for grib in range(len(gribs_live)):
-        gribs_live[grib] -= 1
-        if gribs_live[grib] == 0:
-            gribs_live[grib] = 4
-            gribs_live.append(10)
-            gribs_live.append(10)
-    print(hours, len(gribs_live), gribs_live)
