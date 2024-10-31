2e6f5295ebbfc9d7893743eab209ee9096f0d611
Mihanchos <Shofer2007@gmail.com>
2024-10-31 19:00:54 +0300

    new tasks commiting

--- Informatics/graphs_trees.py
+++ Informatics/graphs_trees.py
@@ -1,16 +1,38 @@
-def min_candies(ratings):
-    n = len(ratings)
-    candies = [1] * n
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-    for i in range(1, n):
-        if ratings[i] > ratings[i - 1]:
-            candies[i] = candies[i - 1] + 1
+    def insert(self, value):
+        if value <= self.value:
+            if self.left is None:
+                self.left = TreeNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = TreeNode(value)
+            else:
+                self.right.insert(value)
 
-    for i in range(n - 2, -1, -1):
-        if ratings[i] > ratings[i + 1]:
-            candies[i] = max(candies[i], candies[i + 1] + 1)
+    def to_list(self):
+        left = self.left.to_list() if self.left else None
+        right = self.right.to_list() if self.right else None
+        return [self.value, left, right]
 
-    return sum(candies)
 
-ratings = eval(input())
-print(min_candies(ratings))
+def build_binary_tree(values):
+    if not values:
+        return []
+
+    root = TreeNode(values[0])
+    for value in values[1:]:
+        root.insert(value)
+
+    return root.to_list()
+
+
+# Пример использования
+values = eval(input())
+print(build_binary_tree(values))
